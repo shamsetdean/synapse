a8ed81ad522f9e6dbfe05dff9a08f5065c8f9e0f
Shams&Dean <shamsetdean@gmail.com>
2026-08-02 08:39:32 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  redirect(user ? "/dashboard" : "/login");
+}
